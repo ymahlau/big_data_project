49d9e3f7ae39c31e874ba6dc89cf896111e8204c
xtremey <mahlauyannik@gmail.com>
2022-10-18 19:11:21 +0200

    Installation of DataDiscoveryBenchmark is now editable

--- test_installation.py
+++ test_installation.py
@@ -6,6 +6,6 @@ def main():
     # load_dresden_db(con, parts=[0,1])
     load_git_tables_db(con)
     df = con.execute('SELECT * FROM AllTables LIMIT 5').fetch_df()
-
+    print(df)
 if __name__ == '__main__':
     main()
