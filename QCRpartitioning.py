09e0a5c463a301e4d62c585d7190c12cd9bfc9a4
zainl <lukas.zain98@gmail.com>
2022-11-03 12:07:49 +0100

    Comments and Joinability Function added

--- QCRpartitioning.py
+++ QCRpartitioning.py
@@ -1,5 +1,5 @@
-
-def tk(sketch:list((str,float))) -> list((str, float)):
+""" returns tuple of lists containing result[0] = T+ and result[1] containing T- """ 
+def tk(sketch:list((str,float))) -> tuple(list(str),list(str)):
     result = ([str],[str])
     for x in sketch:
         for y in sketch:
@@ -11,3 +11,17 @@ def tk(sketch:list((str,float))) -> list((str, float)):
                 sketch.remove(x)
                 sketch.remove(y)
     return result
+
+"""Calculates Joinability of two given input lists of keys unique within their list"""
+def calculateJoinability(KC:list(str),KQ:list(str)) -> float:
+    lenKC = len(KC)
+    lenJoin = 0
+    for x in KQ:
+        for y in KC:
+            if(x==y):
+                lenJoin += 1
+    return lenJoin/lenKC
+
+
+def implementWeighting(joinability: float, correlation_coefficient:float, alpha_j, alpha_r:float) -> float:
+    pass
