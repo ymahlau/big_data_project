97c6aeab4c139df1c201041baed1118c0c573fad
xtremey <mahlauyannik@gmail.com>
2022-11-26 16:34:24 +0100

    Fixed Python parallel implementation

--- qcr_python_parallel.py
+++ qcr_python_parallel.py
@@ -1,25 +1,38 @@
 import math
+import os
+import pickle
 import time
 from collections import defaultdict
 from multiprocessing import Pool
 from pathlib import Path
 from typing import List, Any, Tuple
 import itertools
+
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import duckdb
-import numpy as np
 
+from datadiscoverybench.utils import load_dresden_db
 from qcr import save_index, get_kc, get_c, create_sketch, hash_function, generate_term_keys, get_table_id, \
     add_to_inverted_index, load_index
 
-con = None
 SKETCH_SIZE = 100
-MIN_NUM_COLS = 20
+MIN_NUM_ROWS = 1
+NUM_WORKER = 4
+PARTITION_SIZE = 100
+
+stats_file_path = Path(__file__).parent / 'stats.txt'
+# database connection
+print(f'Process: {os.getpid()} started connecting to db')
+database_path = "/home/groupb/database_v051.db"
+# con = duckdb.connect(database=database_path)
+con = duckdb.connect(database=':memory:')
+load_dresden_db(con, parts=[0, 1])
 
 def get_column_pairs(table) -> List[pd.DataFrame]:
     kc_list = table.select_dtypes(include=["object"]).columns.to_list()
-    c_list = table.select_dtypes(include=["float64", "int64"]).columns.to_list()
+    c_list = table.select_dtypes(include=["float64", "int64", np.number]).columns.to_list()
     column_pairs = list(itertools.product(kc_list, c_list))
     pair_list = []
     for kc, c in column_pairs:
@@ -27,8 +40,11 @@ def get_column_pairs(table) -> List[pd.DataFrame]:
     return pair_list
 
 
-def build_index_part(table_id_lower: int, table_id_upper: int) -> dict:
-    load_df_query = f"select * from allTables where tableID >= {table_id_lower} and tableID < {table_id_upper}"
+def build_index_part(partitions: pd.DataFrame) -> dict:
+    table_id_list = partitions.iloc[:, 0].values.tolist()
+    table_id_str = str(table_id_list).replace('[', '(').replace(']', ')')
+    load_df_query = f"select * from allTables where tableID in {table_id_str}"
+    global con
     df = con.execute(load_df_query).fetch_df()
 
     # grouped = df.sort_values(by=['TableId', 'RowId', 'ColumnId']).set_index(
@@ -36,10 +52,10 @@ def build_index_part(table_id_lower: int, table_id_upper: int) -> dict:
 
     grouped = df.groupby('TableId')
     table_dict = {}
-    for table_id in range(table_id_lower, table_id_upper):
+    for table_id in table_id_list:
         table_stacked = grouped.get_group(table_id)
         table = table_stacked.drop('TableId', axis=1).set_index(['RowId', 'ColumnId']).unstack(-1).droplevel(-2, axis=1)
-        if table.shape[0] >= MIN_NUM_COLS:
+        if table.shape[0] >= MIN_NUM_ROWS:
             table_dict[table_id] = table
 
     inverted_index = defaultdict(set)
@@ -55,59 +71,49 @@ def build_index_part(table_id_lower: int, table_id_upper: int) -> dict:
     return inverted_index
 
 
-def build_index_parallel(con, num_workers: int, task_args, index_path: str):
+def build_index_parallel(partitions, index_path: str):
     print('starting building index')
     start_time = time.time()
-    p = Pool(num_workers)
-    result_dict = defaultdict(set)
+    with Pool(NUM_WORKER) as pool:
+        inverted_index = defaultdict(set)
+        result_list = [pool.apply_async(build_index_part, (part,)) for part in partitions]
 
-    result = p.starmap(build_index_part, task_args)
-    try:
-        task = p._cache[result._job]
-        while task._number_left > 0:
-              # track progress
-            print("Tasks remaining = {0}".format(task._number_left * task._chunksize))
-            time.sleep(10)
-    except:
-        print('done with index building, starting merge')
-
-    for dict_part in result:
-        for k, v in dict_part:
-            result_dict[k].add(v)
-    save_index(result_dict)
+        for res_dict_wrapper in tqdm(result_list):
+            res_dict_wrapper.wait()
+            res_dict = res_dict_wrapper.get()
+            for k, v in res_dict:
+                inverted_index[k].add(v)
+    with open(index_path, "wb") as f:
+        pickle.dump(inverted_index, f)
 
     end_time = time.time()
     runtime = end_time - start_time
     print(f'runtime: {runtime}')
-    with open('/home/groupb/big_data_project/stats_temp.txt', 'w') as f:
+    with open(stats_file_path, 'w') as f:
         f.write(f'Finished in {runtime}s')
 
 
 def build_dresden():
-    database_path = "/home/groupb/database_v051.db"
-    index_path = "/home/groupb/python_parallel_index.pickle"
-    # num_tables = 145533822
-    # partition_size = 1000
-    # num_workers = 64
-    num_tables = 100000
-    partition_size = 100
-    num_workers = 4
-    num_partitions = math.floor(num_tables / partition_size) + 1
+    # index_path = "/home/groupb/python_parallel_index.pickle"
+    index_path = "index.pickle"
 
-    partition_bounds = list(partition_size * np.arange(num_partitions - 1))
-    partition_bounds.append(num_tables)
-    partitions_lower = partition_bounds[:-1]
-    partitions_upper = partition_bounds[1:]
-
-    print('started connecting to db')
+    print('started fetching part list')
     global con
-    con = duckdb.connect(database=database_path)
-    tasks = list(zip(partitions_lower, partitions_upper))
-    build_index_parallel(con, num_workers, tasks, index_path)
+    id_df = con.execute("select distinct TableId from AllTables").fetch_df()
+    num_tables = id_df.shape[0]
+    if num_tables % PARTITION_SIZE == 0:
+        num_partitions = num_tables / PARTITION_SIZE
+    else:
+        num_partitions = math.floor(num_tables / PARTITION_SIZE) + 1
+    partitions = []
+    for i in range(num_partitions):
+        partitions.append(id_df.iloc[i*PARTITION_SIZE:(i+1)*PARTITION_SIZE, :])
+
+    build_index_parallel(partitions[:100], index_path)
 
 
 if __name__ == '__main__':
-    # build_dresden()
+    build_dresden()
     idx = load_index()
     print(len(idx))
     pass
